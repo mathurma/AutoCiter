71d80c003aa349b0c480d3811be3a2607586a9a9
dctisdal <dctisdal@calpoly.edu>
2021-05-18 14:15:42 -0700

    added pbio dataset, work will continue on colab

--- build_plos.py
+++ build_plos.py
@@ -1,15 +1,35 @@
 import bs4 as bs
 import os
 import codecs
+from tqdm import tqdm
 
-plos_fold = "plos/allofplos_xml/"
+fold = "pbio/"
+title_dict = {}
 
-for path in os.listdir(plos_fold):
-    file = codecs.open(plos_fold+path, 'r', encoding="utf-8")
+for i in tqdm(range(len(os.listdir(fold))), desc='Creating Article Title Dict'):
+    path = os.listdir(fold)[i]
+    file = codecs.open(fold+path, 'r', encoding="utf-8")
     file = bs.BeautifulSoup(file, "xml")
-    # build dict of label to ariticle title (maybe take note of author)
+    # build dict of article title to path
+    title_dict[file.find('article-title').get_text()] = path
+
+for i in tqdm(range(len(os.listdir(fold))), desc='Finding Sources in Scope'):
+    path = os.listdir(fold)[i]
+    file = codecs.open(fold+path, 'r', encoding="utf-8")
+    file = bs.BeautifulSoup(file, "xml")
     for ref in file.find_all("ref"):
-        print(ref)
-        if ref.get('id') not in os.listdir(plos_fold):
+        try:
+            title_dict[ref]
+            print(ref)
+        except:
+            continue
+
+print(title_dict)
+
+
+'''
+        for ref in file.find_all("ref"):
+        if ref.get('id') not in os.listdir(fold):
             print("not in plos" + ref.get('id'))
-    break
+'''
+
